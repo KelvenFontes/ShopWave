15341bccc18107f2fc91e90651cb93ae0f8c58db
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-27 02:28:10 -0300

    feat: add page product

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from "@/providers/formatCurrency";
 import Image from "next/image";
 
 const getProductDetails = async (productId: string) => {
@@ -17,14 +18,44 @@ const ProductDetails = async ({ params }: { params: { productId: string } }) => 
   if (!product) return null;
 
   return (
-    <div className="container mx-auto lg:px-40 lg:pt-10">
+    <div className="container mx-auto mt-5 lg:px-40 lg:pt-10">
+      <div className="relative h-[300px] w-full">
+        {product.imagem ? (
+          <Image src={product.imagem} alt={product.nome} layout="fill" objectFit="cover" />
+        ) : (
+          <Image src="/produto-sem-imagem.png" alt="Default Product" layout="fill" objectFit="cover" />
+        )}
+      </div>
+      <div className="mt-5">
+        <h1 className="text-3xl font-semibold">{product.nome}</h1>
+        <p className="text-gray-600 mt-2">{product.descricao}</p>
+        <p className="text-primaryDarker text-2xl font-bold mt-3">{formatPrice(product.preco)}</p>
+        <button className="mt-4 bg-primary hover:bg-primaryDarker text-white px-6 py-3 rounded-md font-semibold transition duration-300">
+          Add to Cart
+        </button>
+      </div>
 
-      {product.imagem ? (
-        <Image src={product.imagem} alt={product.nome} height={200} width={200} />
-      ) : (
-        <Image src="/produto-sem-imagem.png" alt="Default Product" height={200} width={200} />
-      )}
-      <h1>{product.nome}</h1>
+      <div className="mt-8">
+        <h2 className="text-2xl font-semibold mb-4">Description</h2>
+        <p className="text-gray-700">{product.descricao}</p>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="text-2xl font-semibold mb-4">Product Details</h2>
+        <table className="w-full">
+          <tbody>
+            <tr>
+              <td className="font-semibold pr-4">Height:</td>
+              <td>{product.altura} cm</td>
+            </tr>
+            <tr>
+              <td className="font-semibold pr-4">Weight:</td>
+              <td>{product.peso} kg</td>
+            </tr>
+            {/* Adicione mais linhas de informações aqui */}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
